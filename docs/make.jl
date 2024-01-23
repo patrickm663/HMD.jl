c45c0b0a10ad386b3a0c06d8e3666717b562579a
patrickm663 <rev0cation2025@outlook.com>
2024-01-23 09:25:39 +0200

    CICD: reformat make.jl and remove unused dependencies

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,19 @@
-push!(LOAD_PATH,"../src/")
-
+#push!(LOAD_PATH,"../src/")
+# source: https://github.com/JuliaActuary/LifeContingencies.jl/blob/master/docs/make.jl
+#
 using Documenter, HMD
 
-makedocs(sitename = "HMD.jl", modules = [HMD])
+makedocs(;
+	 modules=[HMD],
+	 format=Documenter.HTML(;
+				prettyurls=get(ENV, "CI", "false") == "true",
+				assets=String[]
+			       ),
+	 pages=[
+		"Home" => "index.md",
+		],
+	 repo=Remotes.GitHub("patrickm663", "HMD.jl"),
+	 sitename="HMD.jl",
+	 authors="Patrick Moehrke <patrick@patrickmoehrke.com> and contributors")
 
-deploydocs(repo="github.com/patrickm663/HMD.jl")
+deploydocs(; repo="github.com/patrickm663/HMD.jl")
